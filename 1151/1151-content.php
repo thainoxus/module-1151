426cc3de89bd065d128f3045d243ac979fd6d7a3
thainoxus <thainoxus@gmail.com>
2019-04-10 13:27:44 +0700

    first commit

---
+++ 1151/1151-content.php
@@ -0,0 +1,39 @@
+<?php
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+?>
+
+        <div class="type-25-30">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-2">
+                    </div>
+                    <div class=" col-md-10">
+                        <div class="page-title">
+                            <div class="title_left">
+                                <a href="#"><img src="images/logo_42-1.png"></a>
+                            </div>
+                            <div class="title_right">
+                                <div class="col-md-5 col-sm-5 col-xs-12 pull-right ">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" placeholder="Search for...">
+                                        <span class="input-group-btn">
+                                            <button class="btn btn-default" type="button">Go!</button>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+
+
+        </div>
---
+++ 1151/1151.php
@@ -0,0 +1,44 @@
+<?php
+
+    $url_host = 'http://'.$_SERVER['HTTP_HOST'];
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+    
+    preg_match_all($pattern_uri, __DIR__, $matches);
+    $url_path = $url_host . $matches[1][0];
+    $url_path = str_replace('\\', '/', $url_path);
+
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        require_once($dir_block.'/libs/lessc.inc.php');
+    }
+    
+    $less = new lessc;
+    $less->compileFile('less/25-30.less', 'css/25-30.css');
+    
+?>
+
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="../1151/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="../1151/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/25-30.less', 'css/25-30.css');
+        ?>
+        <link href="../1151/css/25-30.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>   
+            <?php  $dir_block.include'1151-content.php'; ?>
+
+    </body>
+
+</html>
